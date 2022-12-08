d3cffe5a0a1ac8be5549c455e114296eb51c1e63
sanirufee <Muhammedasanya2020@gmail.com>
2022-12-08 15:22:03 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+ 
+/**
+ *main - Print a C program using printf
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	printf("with grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+
+}
