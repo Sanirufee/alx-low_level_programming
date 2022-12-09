351dbaeba85654816445f07dbdaa1dba5736f6dc
sanirufee <Muhammedasanya2020@gmail.com>
2022-12-08 16:27:40 -0800

    task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
-#include <stdio.h> 
+#include <stdio.h
  
 /**
- *main - Print a C program using printf
+ *main - Entry point
  *Return: Always 0 (success)
  */
 
 int main(void)
 
 {
-	printf("with grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 
