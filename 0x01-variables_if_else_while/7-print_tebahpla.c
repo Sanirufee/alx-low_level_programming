cd72d5c2e5abbaeb9a0ece1321ecf93f8afaae1f
sanirufee <Muhammedasanya2020@gmail.com>
2022-12-09 17:29:51 -0800

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
 *main - prints the lowercase alphabet in reverse,
-*followed by a new line 
+*followed by a new line
 *Return: Always 0 (success)
 */
 int main(void)
 {
+
 	char za;
 
 	za = 'z';
 	while (za >= 'a')
-	{
+{
 		putchar(za);
 		za = za - 1;
-	}
+}
 	putchar('\n');
 	return (0);
 }
