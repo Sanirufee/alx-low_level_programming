970e44c2d4e7f4e5f6e1c6b82f9dd10517c15eec
sanirufee <Muhammedasanya2020@gmail.com>
2022-12-13 17:38:42 -0800

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
