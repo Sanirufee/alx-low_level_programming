66d9878ae9faaf54785a274811fe2a832f2f8a4c
sanirufee <Muhammedasanya2020@gmail.com>
2022-12-16 15:19:09 -0800

    task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -17,13 +17,21 @@ int main(void)
 
 	while (n % 2 == 0)
 	{
+		max = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+	{
 		max = i;
 		n = n / i;
 	}
-}
-if (n > 2)
-	max = n;
+	}
+
+	if (n > 2)
+		max = n;
+
 	printf("%ld\n", max);
 	return (0);
-	}
 }
