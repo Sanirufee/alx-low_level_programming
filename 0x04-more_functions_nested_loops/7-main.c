de3ef57972aa1f089d289d77d2ff533611181fd4
sanirufee <Muhammedasanya2020@gmail.com>
2022-12-16 14:01:33 -0800

    task 7

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- *
- *
- * Return: Always 0.
- * int main(void)
- * {
- * int main(void)
- * {
- * print_diagonal(0);
- * print_diagonal(2);
- * print_diagonal(10);
- * print_diagonal(-4);
- *
- * return (0);
- * }
+* main - check the code
+*
+* Return: Always 0
+*/
+int main(void)
+
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
