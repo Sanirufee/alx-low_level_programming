c6d356ba3a11b0ef6776402b12dd15dea7faad64
sanirufee <Muhammedasanya2020@gmail.com>
2022-12-30 16:17:35 -0800

    task 102

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  *rot13 - function that encodes a string using rot13.
- *  @s: pointer pointed to the variable s , of type character
- *  Return: Pointer P , pointed to the variable s of type char.
- *   Update V2.0 : using Pointers instead of arrays to minimize the use
- *   of allocated memory and to improve the esthetic and simplicity of the code.
+ * *rot13 - function that encodes a string using rot13.
+ * @s: pointer pointed to the variable s , of type character
+ * Return: Pointer P , pointed to the variable s of type char.
+ * Update V2.0 : using Pointers instead of arrays to minimize the use
+ * of allocated memory and to improve the esthetic and simplicity of the code.
  */
 char *rot13(char *s)
 {
@@ -13,7 +13,7 @@ char *rot13(char *s)
 
 	char *original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char *code = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
+	
 	while (*s != '\0')
 	{
 		i = 0;
